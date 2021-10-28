5d7021144b27d506d7a5a06ea2a1ea7ce1663d81
yavuzsonmez <contact@yavuzsonmez.com>
2021-10-28 17:50:05 +0200

    update

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 18:44:14 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/10/28 17:40:24 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/10/28 17:49:53 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,22 @@
 
 size_t	parameters(char **argv, t_ph *ph)
 {
-	printf("HELLO\n");
 	ph->param.nb_philo = ft_atoi_ll(argv[1]);
 	ph->param.time_to_die = ft_atoi_ll(argv[2]);
 	ph->param.time_to_eat = ft_atoi_ll(argv[3]);
 	ph->param.time_to_sleep = ft_atoi_ll(argv[4]);
 	if (argv[5] != NULL)
 		ph->param.meals_per_philo = ft_atoi_ll(argv[5]);
-	printf("Nb of philo %d\n", ph->param.nb_philo);
-	//if (ph->param.nb_philo < 0 || ph->param.nb_philo > 2147483647)
-	//	return (1);
-	//else if (ph->param.time_to_die < 0 || ph->param.time_to_die > 2147483647)
-	//	return (1);
-	//else if (ph->param.time_to_eat < 0 || ph->param.time_to_eat > 2147483647)
-	//	return (1);
-	//else if (ph->param.time_to_sleep < 0 || ph->param.time_to_sleep > 2147483647)
-	//	return (1);
-	//else if (argv[5] != NULL && (ph->param.time_to_sleep < 0 || ph->param.time_to_sleep > 2147483647))
-	//	return (1);
+	if (ph->param.nb_philo < 0 || ph->param.nb_philo > 2147483647)
+		return (1);
+	else if (ph->param.time_to_die < 0 || ph->param.time_to_die > 2147483647)
+		return (1);
+	else if (ph->param.time_to_eat < 0 || ph->param.time_to_eat > 2147483647)
+		return (1);
+	else if (ph->param.time_to_sleep < 0 || ph->param.time_to_sleep > 2147483647)
+		return (1);
+	else if (argv[5] != NULL && (ph->param.time_to_sleep < 0 || ph->param.time_to_sleep > 2147483647))
+		return (1);
 	return (0);
 
 }
@@ -47,6 +45,7 @@ int main(int argc, char **argv)
 		return (1);
 	if (parameters(argv, &ph))
 		return (1);
+	/* Loop to create threads */
 	printf("TEST!!\nNumber of philo %d\nTime to die %d\nTime to eat %d\nTime to sleep %d\nMeals per philo %d\n", ph.param.nb_philo, ph.param.time_to_die, ph.param.time_to_eat, ph.param.time_to_sleep, ph.param.meals_per_philo);
 	return (0);
 }
